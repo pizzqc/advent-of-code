38e968cde97d95e2193b42f2e4ebc7b1f6985f54
Antoine Lemieux <antoine.lemieux@cerence.com>
2022-12-11 22:12:14 -0500

    [Day 9] Part 2

--- day9/day9.go
+++ day9/day9.go
@@ -10,16 +10,30 @@ import (
 	"strings"
 )
 
+const (
+	RopeSize int = 10
+)
+
+type CartesianPosition struct {
+	X, Y int
+}
+
+type RopeKnot struct {
+	Position CartesianPosition
+	Number   int
+	Parent   *RopeKnot
+	Next     *RopeKnot
+}
+
 type Matrix struct {
-	StartPos []int
-	HeadPos  []int
-	TailPos  []int
+	StartPos CartesianPosition
+	Head     *RopeKnot
 	Cells    [][]Cell
 	Debug    bool
 }
 
 type Cell struct {
-	x, y         int
+	Position     CartesianPosition
 	visitedCount int
 }
 
@@ -27,10 +41,32 @@ type CellOptions struct {
 	X, Y int
 }
 
+func NewRope(size int) *RopeKnot {
+	head := &RopeKnot{
+		Position: CartesianPosition{X: 0, Y: 0},
+		Number:   0,
+		Parent:   nil,
+		Next:     nil,
+	}
+
+	currentKnot := head
+	for i := 1; i < size; i++ {
+		currentKnot.Next = &RopeKnot{
+			Position: CartesianPosition{X: 0, Y: 0},
+			Number:   i,
+			Parent:   currentKnot,
+			Next:     nil,
+		}
+		currentKnot = currentKnot.Next
+	}
+
+	return head
+
+}
+
 func NewCell(options CellOptions) *Cell {
 	return &Cell{
-		x:            options.X,
-		y:            options.Y,
+		Position:     CartesianPosition{X: options.X, Y: options.Y},
 		visitedCount: 0,
 	}
 }
@@ -51,88 +87,95 @@ func (c *Cell) String() string {
 	return fmt.Sprint(c.visitedCount)
 }
 
-func (m *Matrix) isHeadTailTouch() bool {
-	if m.TailPos[1] == m.HeadPos[1]-1 ||
-		m.TailPos[1] == m.HeadPos[1] ||
-		m.TailPos[1] == m.HeadPos[1]+1 {		
-		if m.TailPos[0] == m.HeadPos[0]-1 ||
-			m.TailPos[0] == m.HeadPos[0] ||
-			m.TailPos[0] == m.HeadPos[0]+1 {
+func (r *RopeKnot) isConnected() bool {
+	if r.Next.Position.Y == r.Position.Y-1 ||
+		r.Next.Position.Y == r.Position.Y ||
+		r.Next.Position.Y == r.Position.Y+1 {
+		if r.Next.Position.X == r.Position.X-1 ||
+			r.Next.Position.X == r.Position.X ||
+			r.Next.Position.X == r.Position.X+1 {
 			return true
 		}
 	}
-
 	return false
 }
 
-func (m *Matrix) markVisited() {
-	m.Cells[m.TailPos[0]][m.TailPos[1]].visitedCount++
+func (r *RopeKnot) shift(direction common.Direction) {
+	currentKnot := r
+
+	switch direction {
+	case common.UP:
+		currentKnot.Position.X++
+		for currentKnot.Next != nil {
+			currentKnot = currentKnot.Next
+			currentKnot.Position.X++
+		}
+	case common.LEFT:
+		currentKnot.Position.Y++
+		for currentKnot.Next != nil {
+			currentKnot = currentKnot.Next
+			currentKnot.Position.Y++
+		}
+	}
+}
+
+func (m *Matrix) markVisited(pos CartesianPosition) {
+	m.Cells[pos.X][pos.Y].visitedCount++
 }
 
 func (m *Matrix) moveHead(direction common.Direction) {
 	switch direction {
 	case common.RIGHT:
-		m.HeadPos[1]++
+		m.Head.Position.Y++
 	case common.UP:
-		m.HeadPos[0]--
+		m.Head.Position.X--
 	case common.LEFT:
-		m.HeadPos[1]--
+		m.Head.Position.Y--
 	case common.DOWN:
-		m.HeadPos[0]++
+		m.Head.Position.X++
 	}
 }
 
-func (m *Matrix) moveTail(direction common.Direction) {
-	switch direction {
-	case common.RIGHT:
-		m.TailPos[1]++
-		if m.TailPos[0] < m.HeadPos[0] {
-			// Go Diag DownRight
-			m.TailPos[0]++
-		} else if m.TailPos[0] > m.HeadPos[0] {
-			// Go Diag UpRight
-			m.TailPos[0]--
-		}
+func (r *RopeKnot) moveKnot() {
+	// Using diff to figure out how to get closer to Parent
+	diffX := r.Position.X - r.Parent.Position.X
+	diffY := r.Position.Y - r.Parent.Position.Y
 
-	case common.UP:
-		m.TailPos[0]--
-		if m.TailPos[1] < m.HeadPos[1] {
-			// Go Diag UpRight
-			m.TailPos[1]++
-		} else if m.TailPos[1] > m.HeadPos[1] {
-			// Go Diag UpLeft
-			m.TailPos[1]--
+	if diffX == 0 { // SAME ROW
+		if diffY < 0 {
+			r.Position.Y++
+		} else {
+			r.Position.Y--
 		}
-
-	case common.LEFT:
-		m.TailPos[1]--
-		if m.TailPos[0] < m.HeadPos[0] {
-			// Go Diag DownLeft
-			m.TailPos[0]++
-		} else if m.TailPos[0] > m.HeadPos[0] {
-			// Go Diag UpLeft
-			m.TailPos[0]--
+	} else if diffX > 0 { // 1+ ROW BELOW
+		if diffY < 0 {
+			r.Position.X--
+			r.Position.Y++
+		} else if diffY == 0 {
+			r.Position.X--
+		} else if diffY > 0 {
+			r.Position.X--
+			r.Position.Y--
 		}
-	case common.DOWN:
-		m.TailPos[0]++
-		if m.TailPos[1] < m.HeadPos[1] {
-			// Go Diag UpRight
-			m.TailPos[1]++
-		} else if m.TailPos[1] > m.HeadPos[1] {
-			// Go Diag UpLeft
-			m.TailPos[1]--
+	} else if diffX < 0 { // 1+ ROW BELOW
+		if diffY < 0 {
+			r.Position.X++
+			r.Position.Y++
+		} else if diffY == 0 {
+			r.Position.X++
+		} else if diffY > 0 {
+			r.Position.X++
+			r.Position.Y--
 		}
 	}
-	m.markVisited()
 }
 
 func (m *Matrix) Right(count int) {
 	fmt.Printf("\n== R %v ==\n", count)
 	for i := 1; i <= count; i++ {
 		// Check if we need to increase matrix size to the right (i.e.: ..H)
-		if m.HeadPos[1] == len(m.Cells[m.HeadPos[0]])-1 {
+		if m.Head.Position.Y == len(m.Cells[m.Head.Position.X])-1 {
 			m.increaseMatrix(common.RIGHT)
-			m.Print()
 		}
 
 		m.Move(common.RIGHT)
@@ -143,9 +186,8 @@ func (m *Matrix) Up(count int) {
 	fmt.Printf("\n== U %v ==\n", count)
 	for i := 1; i <= count; i++ {
 		// Check if we need to increase matrix size to the top
-		if m.HeadPos[0] == 0 {
+		if m.Head.Position.X == 0 {
 			m.increaseMatrix(common.UP)
-			m.Print()
 		}
 
 		m.Move(common.UP)
@@ -156,9 +198,8 @@ func (m *Matrix) Down(count int) {
 	fmt.Printf("\n== D %v ==\n", count)
 	for i := 1; i <= count; i++ {
 		// Check if we need to increase matrix size to the bottom
-		if m.HeadPos[0] == len(m.Cells)-1 {
+		if m.Head.Position.X == len(m.Cells)-1 {
 			m.increaseMatrix(common.DOWN)
-			m.Print()
 		}
 
 		m.Move(common.DOWN)
@@ -169,9 +210,8 @@ func (m *Matrix) Left(count int) {
 	fmt.Printf("\n== L %v ==\n", count)
 	for i := 1; i <= count; i++ {
 		// Check if we need to increase matrix size to the Left
-		if m.HeadPos[1] == 0 {
+		if m.Head.Position.Y == 0 {
 			m.increaseMatrix(common.LEFT)
-			m.Print()
 		}
 
 		m.Move(common.LEFT)
@@ -182,12 +222,19 @@ func (m *Matrix) Left(count int) {
 func (m *Matrix) Move(direction common.Direction) {
 	// Move: HEAD
 	m.moveHead(direction)
-	m.Print()
 
-	// Move: TAIL
-	if !m.isHeadTailTouch() {
-		m.moveTail(direction)
-		m.Print()
+	// Move: Tail Knots
+	currentKnot := m.Head
+	for currentKnot.Next != nil {
+		if !currentKnot.isConnected() {
+			currentKnot.Next.moveKnot()
+
+			// Check if we moved the Tail of the Rope
+			if currentKnot.Next.Next == nil {
+				m.markVisited(currentKnot.Next.Position)
+			}
+		}
+		currentKnot = currentKnot.Next
 	}
 }
 
@@ -201,12 +248,11 @@ func (m *Matrix) increaseMatrix(direction common.Direction) {
 			m.Cells[i] = append(row, *nc)
 		}
 	case common.UP:
-		nr := NewRow(len(m.Cells), len(m.Cells[m.HeadPos[0]]))
+		nr := NewRow(len(m.Cells), len(m.Cells[m.Head.Position.X]))
 		m.Cells = append([][]Cell{nr}, m.Cells...)
-		// Shift Start, Head and Tail X-axis by 1 index down
-		m.StartPos[0]++
-		m.HeadPos[0]++
-		m.TailPos[0]++
+		m.StartPos.X++
+		m.Head.shift(common.UP)
+
 	case common.LEFT:
 		for i, row := range m.Cells {
 			nc := NewCell(CellOptions{
@@ -214,11 +260,10 @@ func (m *Matrix) increaseMatrix(direction common.Direction) {
 				Y: 0})
 			m.Cells[i] = append([]Cell{*nc}, row...)
 		}
-		m.StartPos[1]++
-		m.HeadPos[1]++
-		m.TailPos[1]++
+		m.StartPos.Y++
+		m.Head.shift(common.LEFT)
 	case common.DOWN:
-		nr := NewRow(len(m.Cells), len(m.Cells[m.HeadPos[0]]))
+		nr := NewRow(len(m.Cells), len(m.Cells[m.Head.Position.X]))
 		m.Cells = append(m.Cells, nr)
 	}
 }
@@ -228,19 +273,20 @@ func (m *Matrix) Print() {
 		fmt.Println("")
 		for i, row := range m.Cells {
 			var line strings.Builder
+			overlap := make(map[string][]RopeKnot)
 			for j, col := range row {
-				if m.isHead(i, j) && m.isTail(i, j) && m.isStart(i, j) {
-					line.WriteString("(HTs)")
-				} else if m.isHead(i, j) && m.isTail(i, j) && !m.isStart(i, j) {
-					line.WriteString("(HT)")
-				} else if m.isHead(i, j) && !m.isTail(i, j) && m.isStart(i, j) {
-					line.WriteString("(Hs)")
-				} else if !m.isHead(i, j) && m.isTail(i, j) && m.isStart(i, j) {
-					line.WriteString("(Ts)")
-				} else if m.isHead(i, j) {
-					line.WriteString("H")
-				} else if m.isTail(i, j) {
-					line.WriteString("T")
+				knotFound := m.knotsFound(i, j)
+				if len(knotFound) > 0 {
+					knotIdx := knotFound[0].Number
+					knotName := strconv.Itoa(knotIdx)
+					if knotIdx == 0 {
+						knotName = "H"
+					}
+
+					line.WriteString(knotName)
+					if len(knotFound) > 1 {
+						overlap[knotName] = knotFound[1:]
+					}
 				} else if m.isStart(i, j) {
 					line.WriteString("s")
 				} else if col.visitedCount > 0 {
@@ -249,22 +295,54 @@ func (m *Matrix) Print() {
 					line.WriteString(".")
 				}
 			}
-			fmt.Println(line.String())
+			fmt.Print(line.String())
+			// Print covers
+			for key, over := range overlap {
+				coversList := []string{}
+				for _, knot := range over {
+					coversList = append(coversList, strconv.Itoa(knot.Number))
+				}
+				if m.isStart(over[0].Position.X, over[0].Position.Y) {
+					coversList = append(coversList, "s")
+				}
+
+				fmt.Printf("  (%s covers %v)", key, strings.Join(coversList, ","))
+			}
+			fmt.Println("")
 		}
 		fmt.Println("")
 	}
 }
 
-func (m *Matrix) isHead(i, j int) bool {
-	return m.HeadPos[0] == i && m.HeadPos[1] == j
-}
+func (m *Matrix) knotsFound(i, j int) []RopeKnot {
+	knotsFound := []RopeKnot{}
+	currentKnot := m.Head
+	if currentKnot.Position.X == i && currentKnot.Position.Y == j {
+		knotsFound = append(knotsFound, *currentKnot)
+	}
 
-func (m *Matrix) isTail(i, j int) bool {
-	return m.TailPos[0] == i && m.TailPos[1] == j
+	for currentKnot.Next != nil {
+		currentKnot = currentKnot.Next
+		if currentKnot.Position.X == i && currentKnot.Position.Y == j {
+			knotsFound = append(knotsFound, *currentKnot)
+		}
+	}
+
+	return knotsFound
 }
 
 func (m *Matrix) isStart(i, j int) bool {
-	return m.StartPos[0] == i && m.StartPos[1] == j
+	return m.StartPos.X == i && m.StartPos.Y == j
+}
+
+func (r *RopeKnot) size() int {
+	size := 1
+	for r.Next != nil {
+		size++
+		r = r.Next
+	}
+
+	return size
 }
 
 func main() {
@@ -276,21 +354,19 @@ func main() {
 	scanner := bufio.NewScanner(inputFile)
 
 	m := &Matrix{
-		StartPos: []int{0, 0},
-		HeadPos:  []int{0, 0},
-		TailPos:  []int{0, 0},
+		StartPos: CartesianPosition{X: 0, Y: 0},
+		Head:     NewRope(10),
 		Cells: [][]Cell{
 			{
 				Cell{
-					x:            0,
-					y:            0,
+					Position:     CartesianPosition{X: 0, Y: 0},
 					visitedCount: 1,
 				},
 			},
 		},
 		Debug: false,
 	}
-
+	fmt.Printf("Rope Size: %v\n", m.Head.size())
 	m.Print()
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -307,18 +383,17 @@ func main() {
 		case "D":
 			m.Down(count)
 		}
+		m.Print()
 	}
 
-	// Part1 : How many positions does the tail of the rope visit at least once?
-	part1Result := 0
+	// Part1 + Part2 : How many positions does the tail of the rope visit at least once?
+	tailVisitedCount := 0
 	for _, row := range m.Cells {
 		for _, cell := range row {
 			if cell.visitedCount >= 1 {
-				part1Result++
+				tailVisitedCount++
 			}
 		}
 	}
-	m.Debug = true
-	m.Print()
-	fmt.Printf("Part1 Answer: %v", part1Result)
+	fmt.Printf("Tail Visited at least once: %v", tailVisitedCount)
 }
